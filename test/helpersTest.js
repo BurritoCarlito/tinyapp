ea1fa1a394b8560cb7d12329601be5c8c88f1659
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-05-12 04:06:55 +0000

    updated

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const { getUsersByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUsersByEmail', () => {
+  it('should return a user with a valid email', function() {
+    const user = getUsersByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.strictEqual(user.id, expectedUserID);
+  });
+
+  it('should return user when given an valid email', () => {
+    const user = getUsersByEmail("user2@example.com", testUsers);
+    const expectedUserID = "user2RandomID";
+    assert.deepEqual(user.id, expectedUserID);
+  });
+});
